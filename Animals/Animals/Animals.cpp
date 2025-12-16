db8e1c0201e7789153d3a36157ee04446667496f
SnakeJkey <nikita437465@gmail.com>
2025-12-16 09:06:34 +0100

    Animals a cast mainu

--- Animals/Animals/Animals.cpp
+++ Animals/Animals/Animals.cpp
@@ -1,1 +1,26 @@
 #include "Animals.h"
+#include <iostream>
+using namespace std;
+
+Animals::Animals(const string& name, int age, int weight)
+	: mName(name), mAge(age), mWeight(weight)
+{
+	cout << "Animal constructor called for " << mName << '\n';
+}
+
+Animals::~Animals() = default;
+
+const string& Animals::getName() const
+{
+	return mName;
+}
+
+int Animals::getAge() const
+{
+	return mAge;
+}
+
+void Animals::speak() const
+{
+	cout << getName() << " vydava zvuk \n";
+}
--- Animals/Animals/Animals.h
+++ Animals/Animals/Animals.h
@@ -1,9 +1,17 @@
 #pragma once
+#include <string>
+using namespace std;
 class Animals
 {
-};
-
-class Animals
-{
+public:
+	Animals(const string& name, int age, int weight);
+	virtual ~Animals();
+	const string& getName() const;
+	int getAge() const;
+	virtual void speak() const;
+protected:
+	string mName;
+	int mAge;
+	int mWeight;
 };
 
