1e2a2ac9d1533bf98a31a92610f3711e4a8e2169
GitCat6-7 <kadlec.martin@sosasou.cz>
2025-12-16 09:06:11 +0100

    kocka

--- Animals/Animals/Kocka.cpp
+++ Animals/Animals/Kocka.cpp
@@ -1,1 +1,6 @@
+#include "iostream"
 #include "Kocka.h"
+
+Kocka::Kocka(const std::string& jmeno,
+	int vek,)
+
--- Animals/Animals/Kocka.h
+++ Animals/Animals/Kocka.h
@@ -1,6 +1,15 @@
 #pragma once
+#include "Animals.h"
 class Kocka :
     public Animals
 {
+
+public:
+
+    Kocka(const std::string&, int vek, int vaha)
+        :Animals(name, age, weight)
+
+        ~Kocka() override;
+
 };
 
