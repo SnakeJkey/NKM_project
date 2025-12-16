96c4c3d37f485587175a13c1ab1a77f18a97229b
QuestForPerfection <cerny.marek@sosasou.cz>
2025-12-16 09:36:31 +0100

    had

--- Animals/Animals/Had.cpp
+++ Animals/Animals/Had.cpp
@@ -1,1 +1,40 @@
+//Had.cpp
+
 #include "Had.h"
+#include <iostream>
+#include <string>
+
+
+Had::Had(const std::string& name,
+	int age,
+	int weight,
+	bool isPoisonous,
+	size_t lifeNumber)
+	: Animals(name, age, weight)
+{
+	mLifeNumber = lifeNumber;
+	mIsPoisonous = isPoisonous;
+	std::cout << "Had byl vytvoren: " << name << std::endl;
+}
+
+Had::~Had() = default;
+
+const string& Had::getName() const
+{
+	return mName;
+}
+
+void Had::speak() const
+{
+	std::cout << getName() << "SSSSSSSSSS" << std::endl;
+}
+
+bool Had::isPoisunous() const
+{
+	return mIsPoisonous;
+}
+
+size_t Had::getLifeNumber() const
+{
+	return mLifeNumber;
+}
--- Animals/Animals/Had.h
+++ Animals/Animals/Had.h
@@ -1,6 +1,34 @@
+//Had.h
 #pragma once
+#include "Animals.h"
+#include <string>
+#include <cstddef>  
+
 class Had :
     public Animals
 {
+public:
+
+    Had(const std::string& name,
+        int age,
+        int weight,
+        bool isPoisunous,
+        size_t lifeNumber);
+
+    ~Had() override;
+
+    const std::string& getName() const;
+    void speak() const override;
+    bool isPoisunous() const;
+    size_t getLifeNumber() const;
+
+
+protected:
+    size_t getLifeNumber() const;
+
+
+private:
+    size_t mLifeNumber;
+    bool mIsPoisonous;
 };
 
